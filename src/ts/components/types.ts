6c50852cc40316a55be8bdfc0504626d3d8b7638
Olga Chernega <eliang.cheng@gmail.com>
2022-12-26 20:38:09 +0200

    feat: cart products add/remove, filters view

--- src/ts/components/types.ts
+++ src/ts/components/types.ts
@@ -3,11 +3,21 @@ export type Product = {
   title: string;
   description: string;
   price: number;
+  discountPrice: number;
   discountPercentage: number;
   rating: number;
   stock: number;
-  color?: string;
+  color: string;
   brand: string;
   category: string;
   images: string[];
 };
+
+export interface ProductItem {
+  image: string,
+  title: string;
+  id: number;
+  price: number;
+  discount: number;
+  quantity: number;
+}
